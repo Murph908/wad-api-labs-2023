b0fe69502ae47bdb00eaf9bd8e2a9718a994853a
Murph908 <usho1998@gmail.com>
2023-04-30 18:19:56 +0100

    Topic 12 - Movies context

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -8,6 +8,7 @@ import AuthProvider from "./authContext";
 import AuthHeader from "./authHeader";
 import ProtectedRoutes from "./protectedRoutes";
 import SignUpPage from "./signUpPage";
+import MovieProvider from "./moviesContext";
 
 
 const App = () => {
@@ -29,6 +30,7 @@ const App = () => {
             <Link to="/profile">Profile</Link>
           </li>
         </ul>
+        <MovieProvider>
         <Routes>
           <Route path="/public" element={ <PublicPage /> } />
           <Route path="/" element={ <HomePage /> } />
@@ -43,6 +45,7 @@ const App = () => {
           <Route path="/signup" element={ <SignUpPage /> } />
 
         </Routes>
+        </MovieProvider>
       </AuthProvider>
     </BrowserRouter>
   );
