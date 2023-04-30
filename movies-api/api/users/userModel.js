1a6852e4ade71e5f38af2297163b66f9de8dc0c0
Murph908 <usho1998@gmail.com>
2023-04-30 16:14:26 +0100

    Mongo lab user favourites

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -2,9 +2,30 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+const MovieSchema = new Schema({
+    id: Number,
+    title: String
+  });
+
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  password: {type: String, required: true },
+  favourites: [MovieSchema]
 });
 
+//... Code as before
+
+/*const MovieSchema = new Schema({
+    id: Number,
+    title: String
+  });*/
+  
+  /*const UserSchema = new Schema({
+    username: { type: String, unique: true, required: true},
+    password: {type: String, required: true },
+    favourites: [MovieSchema]
+  });*/
+  
+  // ..code as before
+  
 export default mongoose.model('User', UserSchema);
